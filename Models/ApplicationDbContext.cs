359b4efaf0f90dd9b729d877ce623539e00120c6
bhanu <bhanu.r@thinkbridge.in>
2024-12-20 21:48:07 +0530

    Add project files.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Finshark_api.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        DbSet<Stock> Stocks { get; set; }
+        DbSet<Comment> Comments { get; set; }
+
+    }
+}
