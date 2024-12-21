bed2586f73e7b74bfabba46d60cd3baebf66625a
bhanu <bhanu.r@thinkbridge.in>
2024-12-22 00:00:53 +0530

    Stock controller get and getall

---
+++ Controllers/StockController.cs
@@ -0,0 +1,41 @@
+﻿using Finshark_api.Mappers;
+using Finshark_api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finshark_api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList()
+                        .Select( s=> s.ToStockDto());
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var stock = _context.Stocks.Find(id );
+
+            if(stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock.ToStockDto());
+        }
+
+    }
+}
