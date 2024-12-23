a0c1677df87e8ffc0c1843e632baad2905ba6d87
bhanu <bhanu.r@thinkbridge.in>
2024-12-23 23:56:18 +0530

    interfaces

---
+++ Interfaces/IStockRepository.cs
@@ -0,0 +1,15 @@
+﻿using Finshark_api.Dtos;
+using Finshark_api.Models;
+
+namespace Finshark_api.Interfaces
+{
+    public interface IStockRepository
+    {
+        Task<List<Stock>> GetAllAsync();
+        Task<Stock?> GetByIdAsync(int id);
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateDto stockDto);
+        Task<Stock?> DeleteAsync(int id);
+        Task<bool> StockExists(int id);
+    }
+}
