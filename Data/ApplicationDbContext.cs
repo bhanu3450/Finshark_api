240f985ebc58b50e7f2ddb110219a69ed76f0537
bhanu <bhanu.r@thinkbridge.in>
2024-12-23 23:56:02 +0530

    DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Finshark_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finshark_api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
