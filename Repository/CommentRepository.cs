7f1eca735930dfdae560877370f1feac48d7c26c
bhanu <bhanu.r@thinkbridge.in>
2024-12-23 23:56:55 +0530

    repositories

---
+++ Repository/CommentRepository.cs
@@ -0,0 +1,75 @@
+﻿using Finshark_api.Data;
+using Finshark_api.Dtos;
+using Finshark_api.Interfaces;
+using Finshark_api.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finshark_api.Repository
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Comment> CreateAsync(Comment comment)
+        {
+            await _context.Comments.AddAsync(comment);
+            await _context.SaveChangesAsync();
+            return comment;
+        }
+
+        public async Task<Comment?> DeleteAsync(int id)
+        {
+            var comment = _context.Comments.FirstOrDefault(c => c.Id == id);
+
+            if (comment == null)
+            {
+                return null;
+            }
+
+            _context.Comments.Remove(comment);
+            await _context.SaveChangesAsync();
+
+            return comment;
+        }
+
+        public async Task<List<Comment>> GetAllAsync()
+        {
+            return await _context.Comments.ToListAsync();
+        }
+
+        public async Task<Comment?> GetByIdAsync(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+
+            if (comment == null)
+            {
+                return null;
+            }
+
+            return comment;
+        }
+
+        public async Task<Comment?> UpdateAsync(int id, Comment commentModel)
+        {
+            var existingComment =  await _context.Comments.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingComment == null)
+            { 
+                return null;
+            }
+
+            existingComment.Name = commentModel.Name;
+            existingComment.Content = commentModel.Content;
+
+            await _context.SaveChangesAsync();
+
+            return existingComment;
+        }
+    }
+}
---
+++ Repository/StockRepository.cs
@@ -0,0 +1,75 @@
+﻿using Finshark_api.Data;
+using Finshark_api.Dtos;
+using Finshark_api.Interfaces;
+using Finshark_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finshark_api.Repository
+{
+    public class StockRepository : IStockRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StockRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Stock> CreateAsync(Stock stockModel)
+        {
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stock = await _context.Stocks.FirstOrDefaultAsync(x=> x.Id == id); // first or default returns null
+            
+            if (stock == null) {return null;}
+
+            _context.Stocks.Remove(stock);
+            _context.SaveChangesAsync();
+
+            return stock;
+        }
+
+        public async Task<List<Stock>> GetAllAsync()
+        {
+           return await _context.Stocks.Include(c => c.Comments).ToListAsync();
+
+        }
+
+        public async Task<Stock?> GetByIdAsync(int id)
+        {
+            return await _context.Stocks.Include(c=> c.Comments).FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<bool> StockExists(int id)
+        {
+            return await _context.Stocks.AnyAsync(s => s.Id == id);
+        }
+
+        public async Task<Stock?> UpdateAsync(int id, UpdateDto updateDto)
+        {
+            var existingStock = await _context.Stocks.FirstOrDefaultAsync(x=> x.Id == id);
+
+            if (existingStock == null) 
+            { 
+                return null;
+            }
+
+            existingStock.Purchase = updateDto.Purchase;
+            existingStock.Symbol = updateDto.Symbol;
+            existingStock.Purchase = updateDto.Purchase;
+            existingStock.LastDiv = updateDto.LastDiv;
+            existingStock.MarketCap = updateDto.MarketCap;
+            existingStock.CompanyName = updateDto.CompanyName;
+           
+            await _context.SaveChangesAsync();
+
+            return existingStock;
+
+        }
+    }
+}
